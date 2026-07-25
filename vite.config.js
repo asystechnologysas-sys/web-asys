3ced811409efe3c05eb2c496902c36ca5ad25af4
asystechnologysas-sys <asystechnologysas@asysdigital.com>
2026-07-25 10:31:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html')
-      }
-    }
-  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true
-      }
-    }
-  }
+      '/api': 'http://localhost:3000',
+    },
+  },
 });
