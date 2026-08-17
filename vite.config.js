37109b80fcb2ea97068a1fc740a036b554a4ebde
ASYS TECHNOLOGY S.A.S. <asystechnologysas@asysdigital.com>
2026-08-16 19:11:20 -0500

    agregue la pagina de Politica y Privacidad 5

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html') 
+        admin: resolve(__dirname, 'admin.html'),
+        privacidad: resolve(__dirname, 'politica-privacidad.html'),
+        tratamientoDatos: resolve(__dirname, 'tratamiento-datos.html'),
+        terminosCondiciones: resolve(__dirname, 'terminos-condiciones.html'),
+        politicaCookies: resolve(__dirname, 'politica-cookies.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        terminosServicio: resolve(__dirname, 'terminos-servicio.html')
       }
     }
   },
